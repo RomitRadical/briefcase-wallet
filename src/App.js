6440cfd66e55d2a747aa46be0be7959d7c70889d
Radical <romitradical@protonmail.com>
2019-04-27 15:47:55 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import NavBar from "./components/tools/navbar/navbar";
 import Main from "./screens/main";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     return (
       <div style={styles.container}>
-        <h1>Briefcase</h1>
+        <NavBar isOpen={this.state.collapsed} onClick={this.toggleNavbar} />
         <Main />
       </div>
     );
--- src/screens/main.js
+++ src/screens/main.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { Jumbotron, Button } from "reactstrap";
 
 export default class Main extends Component {
   render() {
     return (
-      <div style={styles.container}>
-        <h1>Main</h1>
+      <div style={styles.text}>
+        <Jumbotron>
+          <h1 className="display-3">Briefcase Wallet</h1>
+          <p className="lead">
+            This is a simple hero unit, a simple Jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <hr className="my-2" />
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
       </div>
     );
   }
@@ -15,5 +30,8 @@ const styles = {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  text: {
+    color: "black"
   }
 };
