cdde0c6d900a89184f5d5d3f4490d41d7e921347
Radical <romitradical@protonmail.com>
2019-04-30 17:55:07 +0530

    Implemented send screen

--- src/screens/receive.js
+++ src/screens/receive.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import QRCode from "qrcode.react";
 import { initWallet } from "../scripts/bitcoincash";
 import { Divider } from "semantic-ui-react";
-import QRCode from "qrcode.react";
+import { Button } from "antd";
 
-class Receive extends Component {
+export default class Receive extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -29,16 +30,26 @@ class Receive extends Component {
         <div>
           <h3>Your Bitcoin Cash Address:</h3>
           <QRCode value={addr} />
+          <div>
+            <Button style={styles.button} shape="round">
+              Copy
+            </Button>
+          </div>
         </div>
       </div>
     );
   }
 }
-export default Receive;
 
 const styles = {
   container: {
     textAlign: "center",
     color: "black"
+  },
+  button: {
+    backgroundColor: "#0492CE",
+    color: "white",
+    marginTop: "10px",
+    padding: "0 50px"
   }
 };
--- src/screens/send.js
+++ src/screens/send.js
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import { Divider } from "semantic-ui-react";
+import { Input, Icon, Button } from "antd";
 
 class Send extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: "",
+      amount: ""
+    };
+  }
+
+  onAmountEnter = address => {
+    this.setState({
+      address
+    });
+  };
+
+  onAmountEnter = amount => {
+    this.setState({
+      amount
+    });
+  };
+
   render() {
+    let fiatSymbol = localStorage.getItem("currency");
+    if (!fiatSymbol) {
+      fiatSymbol = "₹";
+    }
     return (
       <div style={styles.container}>
         <Divider style={styles.container} horizontal>
           Send
         </Divider>
+        <Input
+          style={styles.input}
+          prefix={<Icon type="user" />}
+          placeholder="Enter Bitcoin Cash Address"
+          onChange={this.onAddressEnter}
+        />
+        <Input
+          style={styles.input}
+          prefix={fiatSymbol}
+          placeholder="Enter amount to send"
+          onChange={this.onAmountEnter}
+        />
+        <Button style={styles.button} shape="round">
+          Send
+        </Button>
       </div>
     );
   }
@@ -17,8 +57,17 @@ export default Send;
 const styles = {
   container: {
     flex: 1,
-    alignItems: "center",
+    textAlign: "center",
     justifyContent: "center",
     color: "black"
+  },
+  input: {
+    marginTop: "10px"
+  },
+  button: {
+    backgroundColor: "#0492CE",
+    color: "white",
+    marginTop: "15px",
+    padding: "0 50px"
   }
 };
